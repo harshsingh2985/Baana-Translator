9a4e48d008500d9891a4e282c6ef0b202ba61372
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-06-29 22:46:11 +0530

    ex-04 Wiring with textarea inputs
    in the we have completed the ex-04 which is basically wiring textarea inputs

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var input=document.querySelector("#submitbtn");
-
-
-
+var text=document.querySelector("#txt-inputs")
 
 function eventhandler(){
-    console.log('clicked');
+    // console.log('clicked');
+    var txtinput=text.value;
     
+    console.log("clicked",txtinput);
+
+        
 }
 
 
