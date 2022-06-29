90372f90903b5712313cb7709107233d0ffc58fa
Harsh Singh <104265750+harshsingh2985@users.noreply.github.com>
2022-06-30 02:26:23 +0530

    update:fetch api method in the javascript done

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ var input=document.querySelector("#submitbtn");
 var text=document.querySelector("#txt-inputs")
 var output=document.querySelector("#outputdiv");
 
+var url="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function geturl(text){
+    return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"+"?"+"text="+text;
+
+}
 
 function eventhandler(){
     // console.log('clicked');
     var txtinput=text.value;
-    output.innerText="translated"+txtinput;
+    // output.innerText="translated"+txtinput;
 
-    
+    // calling server for processing
+    fetch(geturl(txtinput))
+    .then(Response=>Response.json())
+    .then(json=>console.log(json.contents.translated));
+
+
 }
 
 
